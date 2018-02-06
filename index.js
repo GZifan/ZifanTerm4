badb826cd8b41580bdba7a6b86549efa57404eb3
郭子凡 <guozifan@kakukobons-mbp.wifi.bcit.ca>
2018-02-06 10:05:43 -0800

    first heroku

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+var port = process.env.PORT || 3000;
+
+
+app.get("/",(req, resp)=>{
+    resp.end("HI welcome to my heroku.");
+})
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+    
+    console.log("port os running");
+})
